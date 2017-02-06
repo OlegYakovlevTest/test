f57f6d453058eff0756e7888f3f7c6158bbdc1ad
oleg.yakovlev.test <oleg.yakovlev.06.test@gmail.com>
2017-02-06 23:40:44 +0200

    changed webpack version

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./webpack.config');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = new (require('express'))();
+const app = new express();
 const port = 3000;
 
 const compiler = webpack(config);
 
-// var MongoClient = require('mongodb').MongoClient;
-// var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
+app.use(bodyParser());
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -20,12 +23,12 @@ app.get("/", function(req, res) {
 
 app.post('/test', function(req, res) {
     console.error('/test', req.body);
-    // var url = 'mongodb://localhost:27017/test';
-    // MongoClient.connect(url, function(err, db) {
-    //     assert.equal(null, err);
-    //     console.log("Connected correctly to server.");
-    //     db.close();
-    // });
+    var url = 'mongodb://localhost:27017/test';
+    MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server.");
+        db.close();
+    });
     res.send('response !!!!');
 });
 
